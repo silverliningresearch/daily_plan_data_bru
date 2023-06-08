fc345fc0d1727050c0a5d04fe2a958574a1a7b07
unknown <lb@silverliningresearch.com>
2023-06-08 15:57:52 +0000

    update new data

--- BRU_PROF/data/completed_by_interviewer.js
+++ BRU_PROF/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "08-06-2023 17:47:29"
+        "download_time": "08-06-2023 17:57:29"
     },
     {
         "InterviewerID": 1,

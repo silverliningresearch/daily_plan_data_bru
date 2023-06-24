20ca5480ae0b8f8f8db85449b07558af58f8e078
unknown <lb@silverliningresearch.com>
2023-06-24 22:44:29 +0000

    update new data

--- BRU_PROF/data/interview_data.js
+++ BRU_PROF/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "JFK",
         "AirlineCode": "",
         "InterviewerID": 4,
-        "download_time": "25-06-2023 00:29:06"
+        "download_time": "25-06-2023 00:44:02"
     },
     {
         "InterviewId": "3d5a2376-3636-4fa5-b88d-0ee2001cc4a2",

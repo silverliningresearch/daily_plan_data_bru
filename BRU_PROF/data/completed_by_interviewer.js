ad57973679fcacd09737e37b43aef58326c04ef9
unknown <lb@silverliningresearch.com>
2023-07-05 14:32:33 +0000

    update new data

--- BRU_PROF/data/completed_by_interviewer.js
+++ BRU_PROF/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "05-07-2023 16:11:31"
+        "download_time": "05-07-2023 16:32:07"
     },
     {
         "InterviewerID": 1,
@@ -263,7 +263,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2023-06-16",
-        "completed_interviews": 65
+        "completed_interviews": 64
     },
     {
         "InterviewerID": 2,
@@ -283,7 +283,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2023-07-05",
-        "completed_interviews": 19
+        "completed_interviews": 26
     },
     {
         "InterviewerID": 3,
@@ -1048,7 +1048,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 10,
         "Interview_Date": "2023-04-28",
-        "completed_interviews": 61
+        "completed_interviews": 62
     },
     {
         "InterviewerID": 10,
@@ -1363,7 +1363,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 14,
         "Interview_Date": "2023-07-05",
-        "completed_interviews": 7
+        "completed_interviews": 9
     },
     {
         "InterviewerID": 15,
@@ -1393,7 +1393,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 15,
         "Interview_Date": "2023-07-05",
-        "completed_interviews": 33
+        "completed_interviews": 36
     }
 ]    
 

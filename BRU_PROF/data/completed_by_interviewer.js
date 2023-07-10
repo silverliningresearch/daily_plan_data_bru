e96e9386fd4b91bcb11a3f4654985af55bbeea33
unknown <lb@silverliningresearch.com>
2023-07-10 11:26:26 +0000

    update new data

--- BRU_PROF/data/completed_by_interviewer.js
+++ BRU_PROF/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "10-07-2023 13:05:11"
+        "download_time": "10-07-2023 13:26:01"
     },
     {
         "InterviewerID": 1,
@@ -293,7 +293,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2023-07-10",
-        "completed_interviews": 44
+        "completed_interviews": 50
     },
     {
         "InterviewerID": 3,
@@ -1429,6 +1429,11 @@ let completed_by_interviewer = `[
         "InterviewerID": 13,
         "Interview_Date": "2023-07-08",
         "completed_interviews": 64
+    },
+    {
+        "InterviewerID": 13,
+        "Interview_Date": "2023-07-10",
+        "completed_interviews": 3
     },
     {
         "InterviewerID": 14,
